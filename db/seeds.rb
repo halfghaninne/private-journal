b0235152bb855bcf7d7d6b0992741b9e188947fd
Alex Millatmal <alexandrajmh@gmail.com>
2018-10-02 15:19:57 -0400

    adds db seeds for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+shuri = User.create(email: "shuri@wakanda.gov", password: "wakandaforever", password_confirmation: "wakandaforever")
+hermione = User.create(email: "hermione@magic.gov", password: "verysafepassword", password_confirmation: "verysafepassword")
+
+shuri.entries.create(title: "Why Wakanda should be a leader in international aid", body: "We can. We should. We will.")
+shuri.entries.create(title: "My UN 2018 Global Hackathon experience", body: "It was really cool. Our team won of course.")
+
+hermione.entries.create(title: "Why Muggle education courses are the future of Ministry diplomacy", body: "Muggles rule, bigotry drools.")
